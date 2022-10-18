49ab44d41223c0106370beb580cd3f0c68538202
Hanna Huzava <guzovaann@gmail.com>
2022-10-18 14:18:33 +0200

    feat: show of characters - Rick and Morty

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -21,11 +21,12 @@ mainContainer.classList.add("mainContainer");
 const mainPart1 = document.createElement("div");
 const mainPart2 = document.createElement("div");
 mainPart2.classList.add("characters_container");
+
 mainContainer.append(mainPart1, mainPart2)
 
 
 //sidebar episode
-async function showEpisode() {
+const  showEpisode = async() => {
     const url = 'https://rickandmortyapi.com/api/episode'
     const res = await fetch(url);
     const jsonRes = await res.json();
@@ -33,63 +34,58 @@ async function showEpisode() {
 
 }
 
-showEpisode().then((responseObject) => renderEpisode(responseObject.results));
 const ul = document.createElement("ul");
 sidebar.appendChild(ul);
 
-function renderEpisode(array) {
-    const creatLi = (id) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<a id=${id} href="#">Episode ${id}</a>`;
-        return li;
-    }
-
-    array.forEach((episode) => {
-        ul.appendChild(creatLi(episode.id))
-    });
-
-}
+showEpisode().then((obj) => {
+    ul.innerHTML = obj.results.map(episode => `
+                <li>
+                    <a id=${episode.id} href="#">Episode ${episode.id}</a>
+                </li>`).join("")
+})
 
 
 // the information of the episode
-showEpisode().then((a) => getEpisode(a.results));
-
-function getEpisode(array) {
+showEpisode().then((arr) => {
     ul.addEventListener("click", (event) => {
         const id = event.target.getAttribute('id');
         if (!id) return;
         mainPart1.innerHTML = `
-            <h2>${array[id-1].name}</h2>
-            <span>${array[id-1].air_date}</span>
+            <h2>${arr.results[id-1].name}</h2>
+            <span>${arr.results[id-1].air_date}</span>
             <span>|</span>
-            <span>${array[id-1].episode}</span>`
+            <span>${arr.results[id-1].episode}</span>`
 
 
+        let characters = arr.results[id - 1].characters;
 
-        array[id - 1].characters.forEach(el => fetch(el)
-            .then((response) => response.json())
-            .then((responseObject) => console.log(responseObject))
-        )
-
-
-
-        // function renderCharacters(obj) {
-        //    const div = document.createElement("div");
-        // mainPart2.appendChild(div);
-
-        // div.innerHTML = `<div>
-        //             <img src=${obj.image}></img>
-        //             <h3>${obj.name}</h3>
-        //             <span>${obj.species}</span>
-        //             <span>|</span>
-        //             <span>${obj.status}</span>
-        //             </div>`
-
-
-        // }
+        showCharacters(characters).then((charactersString) => {
+            mainPart2.innerHTML = charactersString;
+        });
     })
+})
+
+const getCharacter = async (url) => {
+    const res = await fetch(url);
+    const jsonRes = await res.json();
+    return jsonRes;
+
 }
 
+const showCharacters = async (characters) => {
+    const resultArray = await Promise.all(characters.map(async (character) => getCharacter(character)));
+    let results = resultArray;
+
+    return results.map((obj) =>
+        `<div class="character">
+            <img src=${obj.image} width="100%" height="150">
+            <h3>${obj.name}</h3>
+            <span>${obj.species}</span>
+            <span>|</span>
+            <span>${obj.status}</span>
+         </div>
+    `).join('')
+}
 
 
 main.append(sidebar, mainContainer);
